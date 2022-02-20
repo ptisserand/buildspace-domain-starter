25d531801a4a7346173f0b9de71b91f8261556f1
Patrice Tisserand <p.tisserand@gmail.com>
2022-02-20 22:07:20 +0100

    Added wallet connect support

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -8,7 +8,9 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
-	const checkIfWalletIsConnected = () => {
+	const [currentAccount, setCurrentAccount ] = useState('');
+
+	const checkIfWalletIsConnected = async () => {
 		const { ethereum } = window;
 		if (!ethereum) {
 			console.log("Make sure you have metamask");
@@ -16,13 +18,38 @@ const App = () => {
 		} else {
 			console.log("We have the ethereum object", ethereum);
 		}
+
+		// check if we have access to wallete
+		const accounts = await ethereum.request({method: 'eth_accounts'});
+		if (accounts.length !== 0) {
+			// if there is multiple account grabd the first one
+			const account = accounts[0];
+			console.log("Found an account:", account);
+			setCurrentAccount(account);
+		} else {
+			console.log("No authorized account found");
+		}
 	}
 
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+			if (!ethereum) {
+				alert("Metamask is required: https://metamask.io");
+				return;
+			}
+			const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+			console.log("Connected:", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 	// function to render if wallet is not connected
 	const renderNotConnectedWallet = () => (
 		<div className='connect-wallet-container'>
 			<img src="https://media.giphy.com/media/xr7GE8l07Zw2Y/giphy.gif" alt="Robin gif" />
-			<button className="cta-button connect-wallet-button">
+			<button onClick={connectWallet} className="cta-button connect-wallet-button">
 				Connect Wallet
 			</button>
 		</div>
@@ -45,7 +72,7 @@ const App = () => {
 					</header>
 				</div>
 
-				{renderNotConnectedWallet()}
+				{!currentAccount && renderNotConnectedWallet()}
 				<div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
 					<a
